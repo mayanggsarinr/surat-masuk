bd21f5d4111c4e34b700119e3238d70fc5ecd143
mayanggsarinr <mayanggsarinr.22@gmail.com>
2026-02-06 05:02:13 +0700

    create surat_sanksi

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 const API_URL =
-  "https://script.google.com/macros/s/AKfycbxe1WmPPjd_p2eh8jrdM5xAFxiqAtNz_AopmHLOEX4r4k-H8Slp0532uhzv51l7EoUOXA/exec";
+  "https://script.google.com/macros/s/AKfycbzUx_PrJAD_EjPsz2PJYkT9VFtTZEpHuPTwIqowG-Clskj_8uLykv1KVmaFcRKV2nlMbw/exec";
