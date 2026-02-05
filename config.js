507a0fd74bb2a07ab8a77935c9a97569a86b538c
mayanggsarinr <mayanggsarinr.22@gmail.com>
2026-02-05 21:33:38 +0700

    -

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 const API_URL =
-  "https://script.google.com/macros/s/AKfycbwjMvX-EewxVafBhNIk3NKEaWuNlv7jN-F6wcNdsMAEh1DMXCDa9DqSuk5mf0gnVuwJYg/exec";
+  "https://script.google.com/macros/s/AKfycbxO4RHYPOdAt7b73yGE8K41zULH_nl65a1CPigfB3ed9V463pmkLVn06grkYffOmNdGGQ/exec";
